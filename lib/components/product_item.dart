4b7bb9cc3b42d6bfc28c804d7a9f313e2c3bff19
Gabrielhlc <gabrielhenlc@gmail.com>
2023-05-29 10:43:02 -0300

    feat: toggle favorite

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,23 +1,25 @@
-import 'package:cishop/pages/product_detail_page.dart';
-import 'package:cishop/utils/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../utils/app_routes.dart';
 
 import '../models/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-  const ProductItem({required this.product, super.key});
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).colorScheme.secondary,
           ),
           trailing: IconButton(
