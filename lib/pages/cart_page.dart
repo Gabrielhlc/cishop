193744f7b7a4e5e8b701ba1d074c90429a0eceb4
Gabrielhlc <gabrielhenlc@gmail.com>
2023-05-30 14:13:05 -0300

    add: cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../components/cart_item.dart';
+
+import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Carrinho'),
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 25,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    label: Text(
+                      'R\$${cart.totalAmount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Theme.of(context)
+                            .primaryTextTheme
+                            .headlineLarge
+                            ?.color,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    child: const Text('COMPRAR'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, index) => CartItemWidget(items[index]),
+            ),
+          ),
+        ],
       ),
     );
   }
