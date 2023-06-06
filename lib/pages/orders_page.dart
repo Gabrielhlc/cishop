3e7d89016d407742386f0847b5d9ace4b9e8bee5
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-06 11:59:10 -0300

    feat: orders_page refreshIndicator

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -9,6 +9,10 @@ import '../models/order_list.dart';
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
+  Future<void> _refreshProducts(BuildContext context) {
+    return Provider.of<OrderList>(context, listen: false).loadOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,27 +20,30 @@ class OrdersPage extends StatelessWidget {
         title: const Text("Meus Pedidos"),
       ),
       drawer: const AppDrawer(),
-      body: FutureBuilder(
-        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.error != null) {
-            return const Center(
-              child: Text('Ocorreu um erro!'),
-            );
-          } else {
-            return Consumer<OrderList>(
-              builder: (ctx, orders, child) => ListView.builder(
-                itemCount: orders.itemsCount,
-                itemBuilder: (ctx, index) =>
-                    OrderWidget(order: orders.items[index]),
-              ),
-            );
-          }
-        },
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.error != null) {
+              return const Center(
+                child: Text('Ocorreu um erro!'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (ctx, orders, child) => ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (ctx, index) =>
+                      OrderWidget(order: orders.items[index]),
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,4 +1,3 @@
-import 'package:cishop/models/product_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
