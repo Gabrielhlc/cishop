b9571fdee44f11fd6e39740c89100e6471c32a96
Gabrielhlc <gabrielhenlc@gmail.com>
2023-06-06 11:15:58 -0300

    feat: toggleFavorite on db

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,11 +5,10 @@ import 'package:http/http.dart' as http;
 
 import '../exceptions/http_exception.dart';
 
+import '../utils/constants.dart';
 import 'product.dart';
 
 class ProductList with ChangeNotifier {
-  final _baseUrl = 'https://cishop-65cba-default-rtdb.firebaseio.com/products';
-
   final List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -24,7 +23,8 @@ class ProductList with ChangeNotifier {
   Future<void> loadProducts() async {
     _items.clear();
 
-    final response = await http.get(Uri.parse('$_baseUrl.json'));
+    final response =
+        await http.get(Uri.parse('${Constants.PRODUCT_BASE_URL}.json'));
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -44,7 +44,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> _addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl.json'),
+      Uri.parse('${Constants.PRODUCT_BASE_URL}.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -94,7 +94,7 @@ class ProductList with ChangeNotifier {
 
     if (index >= 0) {
       await http.patch(
-        Uri.parse('$_baseUrl/${product.id}.json'),
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/${product.id}.json'),
         body: jsonEncode(
           {
             "name": product.name,
@@ -119,7 +119,7 @@ class ProductList with ChangeNotifier {
       notifyListeners();
 
       final response = await http.delete(
-        Uri.parse('$_baseUrl/${product.id}.json'),
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/${product.id}.json'),
       );
 
       if (response.statusCode >= 400) {
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const PRODUCT_BASE_URL =
+      'https://cishop-65cba-default-rtdb.firebaseio.com/products';
+}
